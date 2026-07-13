f01bd4337b8d18f916e8450bcf85166621b2f6da
vothanhtri2212-blip <vothanhtri2212@gmail.com>
2026-07-13 23:58:56 +0700

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> bracketStack;
+        for (int i = 0; i < s.length(); i++) {
+            char current = s[i];
+            if (current == '(' || current == '{' || current == '[') {
+                bracketStack.push(current);
+            } 
+            else {
+                if (bracketStack.empty()) {
+                    return false;
+                }
+                char top = bracketStack.top();
+                if ((current == ')' && top == '(') || 
+                    (current == '}' && top == '{') || 
+                    (current == ']' && top == '[')) {
+                    bracketStack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return bracketStack.empty();
+    }
+};
