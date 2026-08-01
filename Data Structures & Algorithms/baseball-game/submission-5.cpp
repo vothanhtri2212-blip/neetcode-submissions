e1a86303a4e812381e65df88c26d96a53fd3c173
vothanhtri2212-blip <vothanhtri2212@gmail.com>
2026-08-01 14:43:11 +0700

    Add: baseball-game - submission-5

---
+++ Data Structures & Algorithms/baseball-game/submission-5.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int calPoints(vector<string>& operations) {
+        vector<int> record;
+        for (int i = 0; i < operations.size(); i++) {
+            int recordSize = record.size();
+            if (operations[i] == "D") {
+                record.push_back(record[recordSize-1]*2);
+            }
+            else if (operations[i] == "C") {
+                record.pop_back();
+            }
+            else if (operations[i] == "+") {
+                record.push_back(record[recordSize-1] + record[recordSize-2]);
+            }
+            else record.push_back(stoi(operations[i]));
+        }
+        int sum = 0;
+        for (int i = 0; i < record.size(); i++) {
+            sum = sum + record[i];
+        }
+        return sum;
+    }
+};
